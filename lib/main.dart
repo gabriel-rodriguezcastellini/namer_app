aa88321434775d353106fefcdf68b8673cffc3a2
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-04-22 13:03:44 -0300

    Refactor MyHomePage button to call getNext method for idea generation

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
   void getNext() {
     current = WordPair.random();
     notifyListeners();
@@ -45,7 +46,7 @@ class MyHomePage extends StatelessWidget {
           Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext();
             },
             child: Text('Next'),
           ),
